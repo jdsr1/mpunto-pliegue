3b8658acbb02c587504993181df8c634af3c5ae3
jota-de <jota-de@users.noreply.github.com>
2016-05-21 00:12:09 -0600

    Update extra.py

--- extra.py
+++ extra.py
@@ -1,6 +1,6 @@
 class Corriente(object):
     """
-    Una clase para crear objetos tipo Corriente.
+    Una clase para contener las propiedades necesarias para simular una corriente.
     """
     
     def __init__(self, Ti, Tf, WCp):
@@ -8,4 +8,15 @@ class Corriente(object):
         self.Tf = float(Tf) #temperatura final
         self.Tpp= 0 #temperatura del punto de pliegue
         self.WCp = float(WCp)
-        self.cal = Ti > Tf  #Verdadero si la corriente es una corriente caliente
+        self.cal = Ti > Tf  #verdadero si la corriente es una corriente caliente
+    
+def enIntervalo(c, Ta, Tb):
+    """
+    Determina si la corriente c pasa por el intervalo de temperaturas [Ta, Tb].
+    """
+    
+    if max([c.Ti, c.Tf]) > min([Ta, Tb]):
+        if min([c.Ti, c.Tf]) < max([Ta, Tb]):
+            return True
+    else:
+        return False
