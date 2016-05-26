a3d8a07725bdaf5886d8dd92fd143f361fc48c46
jota-de <jota-de@users.noreply.github.com>
2016-05-25 20:14:43 -0600

    Update mppliegue.py
    El archivo se adecuó a las definiciones en corriente.py

--- mppliegue.py
+++ mppliegue.py
@@ -4,120 +4,119 @@
 # mppliegue.py
 # JuanDiego.SR92@gmail.com
 # ------------------------------------------------------------------------------
-# Método de punto de pliegue (Pinch point method) para obtener los servicios   
-# mínimos de calentamiento y enfriamiento necesarios para integrar completamente
-# un sistema de corrientes.
+# Método de punto de pliegue (pinch point) para obtener los servicios mínimos de
+# calentamiento y enfriamiento necesarios para integrar completamente un sistema
+# de corrientes.
 # ------------------------------------------------------------------------------
 
 import numpy as np
 import matplotlib.pyplot as plt
-from corrs import *
+from corriente import *
 
-def mpp(cc, DTmin=10):
-	"""
-	Regresa los servicios de calentamiento y enfriamiento mínimos, así como la
-	temperatura del punto de pliegue para el sistema formado por las corrientes
-	en el vector cc. Modifica la propiedad Tpp de cada corriente en cc.
-	"""
+def mpunto_pliegue(cc, DTmin=20):
+    """
+    Regresa los servicios de calentamiento y enfriamiento mínimos, así como la
+    temperatura del punto de pliegue para el sistema formado por las corrientes
+    en el vector cc. Modifica la propiedad Tpp de cada corriente en cc.
+    """
 
-	vTemp = [] #vector de temperaturas
-	
-	for c in cc:
-		if c.cal == True:
-			#Ajuste de temperaturas para corrientes calientes
-			c.Ti = c.Ti - DTmin/2.0
-			c.Tf = c.Tf - DTmin/2.0
-		else:
-			#Ajuste de temperaturas para corrientes frías
-			c.Ti = c.Ti + DTmin/2.0
-			c.Tf = c.Tf + DTmin/2.0
-		#Agregar las temperaturas ajustadas al vector de temperaturas,
-		#excepto si la temperaturas a agregar ya se encuentra en el vector
-		if c.Ti not in vTemp: vTemp.append(c.Ti)
-		if c.Tf not in vTemp: vTemp.append(c.Tf)
-	
-	#Ordenar el vector de temperaturas de mayor a menor
-	vTemp.sort(reverse=1)
-	
-	WCpInt = [] #WCp por intervalo
-	QInt = []   #Q por intervalo
-	
-	#Determinación del Wcp y Q por intervalo
-	i = 1 #intervalo 1
-	while i < len(vTemp):
-		WCpi = 0
-		for c in cc:
-			#sólo considerar aquellas corrientes que están en el intervalo
-			if enIntervalo(c, vTemp[i-1], vTemp[i]):
-				if c.cal == True:
-					#si la corriente es una corriente caliente
-					#restar su WCp al WCp del intervalo
-					WCpi = WCpi - c.WCp
-				else:
-					#si la corriente es una corriente fría
-					#sumar su WCp al WCp del intervalo
-					WCpi = WCpi + c.WCp
-		#calcular el calor por intervalo
-		Qi = WCpi * (vTemp[i-1] - vTemp[i])
-		WCpInt.append(WCpi) #agregar WCpi al vector de WCp
-		QInt.append(Qi)     #agregar Qi al vector de Q
-		i = i + 1
-	
-	#Cascada de calor. Mismo tamaño que el vector vTemp
-	cascada = [0] * len(vTemp)
-	#Primera iteración. Suponer que el primer valor en la cascada es 0
-	i = 1
-	while i < len(vTemp):
-		cascada[i] = cascada[i-1] - QInt[i-1]
-		i = i + 1
+    vTemp = [] #vector de temperaturas
+    for c in cc:
+        if c.cal == True:
+            #Ajuste de temperaturas para corrientes calientes
+            c.Ti = c.Ti - DTmin/2.0
+            c.Tf = c.Tf - DTmin/2.0
+        else:
+            #Ajuste de temperaturas para corrientes frías
+            c.Ti = c.Ti + DTmin/2.0
+            c.Tf = c.Tf + DTmin/2.0
+        #Agregar las temperaturas ajustadas al vector de temperaturas,
+        #excepto si la temperatura a agregar ya se encuentra en el vector
+        if c.Ti not in vTemp: vTemp.append(c.Ti)
+        if c.Tf not in vTemp: vTemp.append(c.Tf)
+    
+    #Ordenar el vector de temperaturas de mayor a menor
+    vTemp.sort(reverse=1)
+    
+    WCpInt = [] #WCp por intervalo
+    QInt = []   #Q por intervalo
+    
+    #Determinación del Wcp y Q por intervalo
+    i = 1 #intervalo 1
+    while i < len(vTemp):
+        WCpi = 0
+        for c in cc:
+            #sólo considerar aquellas corrientes que están en el intervalo
+            if c.enIntervalo(vTemp[i-1], vTemp[i]):
+                if c.cal == True:
+                    #si la corriente es una corriente caliente
+                    #restar su WCp al WCp del intervalo
+                    WCpi = WCpi - c.WCp
+                else:
+                    #si la corriente es una corriente fría
+                    #sumar su WCp al WCp del intervalo
+                    WCpi = WCpi + c.WCp
+        #calcular el calor por intervalo
+        Qi = WCpi * (vTemp[i-1] - vTemp[i])
+        WCpInt.append(WCpi) #agregar WCpi al vector de WCp
+        QInt.append(Qi)     #agregar Qi al vector de Q
+        i = i + 1
+    
+    #Cascada de calor. Mismo tamaño que el vector vTemp
+    cascada = [0] * len(vTemp)
+    #Primera iteración. Suponer que el primer valor en la cascada es 0
+    i = 1
+    while i < len(vTemp):
+        cascada[i] = cascada[i-1] - QInt[i-1]
+        i = i + 1
 
-	#Comprobación de la cascada de calor. El valor mínimo debería ser un 0
-	while min(cascada) != 0:
-		Qmin = min(cascada)
-		i = 0
-		while i < len(vTemp):
-			#Restar el valor mínimo a cada elemento del vector cascada
-			cascada[i] = cascada[i] - Qmin
-			i = i + 1
-	
-	pp = cascada.index(0) #el punto de pliegue es el lugar (índice) donde hay un cero
-	                      #en la cascada de calor. (Este método no considera múltiples
-	                      #puntos de pliegue)
-	Tpp = vTemp[pp]	      #temperatura del punto de pliegue
-	smcal = cascada[0]    #servicio mínimo de calentamiento
-	smenf = cascada[-1]   #servicio mínimo de enfriamiento
-	
-	#Impresión de la cascada de calor final
-	i = 0
-	print "T: ",
-	while i < len(vTemp)-1:
-		print vTemp[i],'->',
-		i = i + 1
-	print vTemp[i]
-	i = 0
-	print "Q: ",
-	while i < len(cascada)-1:
-		print cascada[i],'->',
-		i = i + 1
-	print cascada[i]
-	
-	#Restaurar las temperaturas de las corrientes a sus valores originales.
-	#Agregar la temperatura de punto de pliegue a cada corriente
-	for c in cc:
-		if c.cal == True:
-			c.Tpp = Tpp + DTmin/2.0
-			c.Ti = c.Ti + DTmin/2.0
-			c.Tf = c.Tf + DTmin/2.0
-		else:
-			c.Tpp = Tpp - DTmin/2.0
-			c.Ti = c.Ti - DTmin/2.0
-			c.Tf = c.Tf - DTmin/2.0
-	
-	#Regresar la temperatura del punto de pliegue y los servicios mínimos
-	round2 = lambda x: round(x,2)
-	return map(round2, [Tpp, smcal, smenf])
+    #Comprobación de la cascada de calor. El valor mínimo debería ser un 0
+    while min(cascada) != 0:
+        Qmin = min(cascada)
+        i = 0
+        while i < len(vTemp):
+            #Restar el valor mínimo a cada elemento del vector cascada
+            cascada[i] = cascada[i] - Qmin
+            i = i + 1
+    
+    pp = cascada.index(0) #el punto de pliegue es el lugar (índice) donde hay un
+                          #cero en la cascada de calor. En caso de múltiples
+                          #puntos de pliegue este método elegirá al primero.
+    Tpp = vTemp[pp]       #temperatura del punto de pliegue
+    smcal = cascada[0]    #servicio mínimo de calentamiento
+    smenf = cascada[-1]   #servicio mínimo de enfriamiento
+    
+    #Impresión de la cascada de calor final
+    i = 0
+    print "T: ",
+    while i < len(vTemp)-1:
+        print vTemp[i],'->',
+        i = i + 1
+    print vTemp[i]
+    i = 0
+    print "Q: ",
+    while i < len(cascada)-1:
+        print cascada[i],'->',
+        i = i + 1
+    print cascada[i]
+    
+    #Restaurar las temperaturas de las corrientes a sus valores originales
+    #Agregar la temperatura de punto de pliegue a cada corriente
+    for c in cc:
+        if c.cal == True:
+            c.Tpp = Tpp + DTmin/2.0
+            c.Ti = c.Ti + DTmin/2.0
+            c.Tf = c.Tf + DTmin/2.0
+        else:
+            c.Tpp = Tpp - DTmin/2.0
+            c.Ti = c.Ti - DTmin/2.0
+            c.Tf = c.Tf - DTmin/2.0
+    
+    #Regresar la temperatura del punto de pliegue y los servicios mínimos
+    round2 = lambda x: round(x,2)
+    return map(round2, [Tpp, smcal, smenf])
 
-def ddc(cc):
+def diagrama_corrientes(cc):
     """
     Una función para visualizar las corrientes dadas en el vector cc, una vez la
     propiedad Tpp de cada corriente ha sido ajustada. Todas las corrientes se
@@ -141,19 +140,23 @@ def ddc(cc):
             plt.plot(x,y,'r<-') #rojo para las calientes
         else:
             plt.plot(x,y,'b>-') #azul para las frías
-        #Anotar calores arriba/debajo del punto de pliegue
+        #Calores arriba/debajo del punto de pliegue
         if c.Qapp != 0:
             if c.cal == True:
-                xm = (c.Tpp+c.Ti)/2
+                if c.Qapp != c.Qtotal: xm = (c.Tpp+c.Ti)/2
+                else: xm = (c.Tf+c.Ti)/2-sep
             else:
-                xm = (c.Tpp+c.Tf)/2
+                if c.Qapp != c.Qtotal: xm = (c.Tpp+c.Tf)/2
+                else: xm = (c.Ti+c.Tf)/2-sep
             ym = i + 0.1
             plt.annotate(c.Qapp, xy=(xm,ym), xytext=(xm,ym))
         if c.Qdpp != 0:
             if c.cal == True:
-                xm = (c.Tpp+c.Tf)/2
+                if c.Qdpp != c.Qtotal: xm = (c.Tpp+c.Tf)/2
+                else: xm = (c.Ti+c.Tf)/2-sep
             else:
-                xm = (c.Tpp+c.Ti)/2
+                if c.Qdpp != c.Qtotal: xm = (c.Tpp+c.Ti)/2
+                else: xm = (c.Tf+c.Ti)/2-sep
             ym = i + 0.1
             plt.annotate(c.Qdpp, xy=(xm,ym), xytext=(xm,ym))
         i = i + 1
